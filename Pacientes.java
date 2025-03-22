7844c662136dd42a26e57e25d07688b742c4ee7a
cristophersantana709 <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2025-03-21 20:05:56 -0500

    Update Pacientes.java

--- Pacientes.java
+++ Pacientes.java
@@ -1,5 +1,35 @@
 package examen_recuperacion;
-
-public class Pacientes {
+import java.io.Serializable;
+public class Pacientes implements Serializable {
+	public String pacienteID;
+	public String nombres;
+	public String apellidos;
+	public String genero;
+	public String tiposangre;
+	public String telefono;
+	public String direccion;
+	public Pacientes(String pacienteID, String nombres, String apellidos, String genero, String tiposangre,
+			String telefono, String direccion) {
+		super();
+		this.pacienteID = pacienteID;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.genero = genero;
+		this.tiposangre = tiposangre;
+		this.telefono = telefono;
+		direccion = direccion;
+	}
+	public Pacientes() {
+		super();
+	}
+	public void Imprimir() {
+		System.out.println("pacienteID: "+this.pacienteID);
+		System.out.println("Nombres: "+this.nombres);
+		System.out.println("apellidos: "+this.apellidos);
+		System.out.println("genero: "+this.genero);
+		System.out.println("telefono: "+this.telefono);
+		System.out.println("direccion: " +this.direccion);
+		System.out.println("Tipo de sangre: "+this.tiposangre);
+	}	
 
 }
